01d58207c966aa6941656766ac8286c8c7c67ae9
rachelmumu <3227007467@qq.com>
2024-01-11 23:00:20 +0800

    go

--- js/01-variable/practice.js
+++ js/01-variable/practice.js
@@ -4,3 +4,10 @@ let jichi = 10
 let sugar = true
 
 console.log("菜名：", caiming)
+
+caiming = "xianglajichi"
+console.log("菜名：",caiming)
+
+cosole.log("可乐：",kele);
+console.log("鸡翅：",jichi);
+console.log("加糖：",sugar);
